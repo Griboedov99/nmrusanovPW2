7bf38e57c98c3bdcc1296656212a51ead0fb86a6
Rusanov Nikita <nmrusanov@edu.hse.ru>
2025-09-21 09:58:57 -0700

    Initial Commit

---
+++ nmrusanovPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nmrusanovPW2
+//
+//  Created by Nick on 21.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
